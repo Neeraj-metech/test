55df91f2c9aea4eba28f1a227472655d607c3f4e
neeraj-metech <neeraj.metkumar@gmail.com>
2019-12-18 12:43:13 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>git tutorials</title>
+</head>
+<body>
+    <h1>Git Tutorials</h1>
+    <ul>
+        <li>list 1</li>
+        <li>list 2</li>
+        <li>list 3</li>
+        <li>list 4</li>
+        <li>list 5</li>`
+        <li>list 6</li>`
+    </ul>
+</body>
+</html>
